0e05f955a5e2e29c1797c4a1367c728c587ef6df
bharathkumar000 <bharathece2006@gmail.com>
2026-02-22 19:09:47 +0530

    v2.4

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,24 @@
-import js from '@eslint/js';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-
 export default [
-    { ignores: ['dist'] },
     {
-        files: ['**/*.{js,jsx}'],
+        ignores: ["dist", "node_modules"],
+    },
+    {
+        files: ["**/*.js", "**/*.jsx"],
         languageOptions: {
-            ecmaVersion: 2020,
+            ecmaVersion: "latest",
+            sourceType: "module",
             globals: {
-                browser: true,
-                es2020: true,
-                node: true,
-            },
-            parserOptions: {
-                ecmaVersion: 'latest',
-                ecmaFeatures: { jsx: true },
-                sourceType: 'module',
-            },
-        },
-        plugins: {
-            react,
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
+                window: "readonly",
+                document: "readonly",
+                localStorage: "readonly",
+                console: "readonly",
+                setTimeout: "readonly",
+                process: "readonly"
+            }
         },
         rules: {
-            ...js.configs.recommended.rules,
-            ...react.configs.recommended.rules,
-            ...react.configs['jsx-runtime'].rules,
-            ...reactHooks.configs.recommended.rules,
-            'react/jsx-no-target-blank': 'off',
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true },
-            ],
-            'react/prop-types': 'off',
-        },
-        settings: { react: { version: '18.2' } },
-    },
+            "no-unused-vars": "warn",
+            "no-undef": "error"
+        }
+    }
 ];
